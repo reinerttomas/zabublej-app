cd5c22e6db66b26bab0f0987a86ec8376fd9ec67
Tomáš Reinert <reinerttomas@gmail.com>
2025-02-24 21:17:04 +0100

    refactor: use command non-interactive mode

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -7,12 +7,13 @@ namespace App\Console\Commands;
 use App\Actions\Users\CreateUserAction;
 use Illuminate\Console\Command;
 use Illuminate\Validation\Rules\Password;
+use InvalidArgumentException;
 
 use function Laravel\Prompts\form;
 
 final class CreateUserCommand extends Command
 {
-    protected $signature = 'user:create';
+    protected $signature = 'users:create {--first_name=} {--last_name=} {--email=} {--password=}';
 
     protected $description = 'Command description';
 
@@ -24,37 +25,49 @@ final class CreateUserCommand extends Command
 
     public function handle(): void
     {
-        $data = form()
-            ->text(
-                label: 'What is your first name?',
-                required: true,
-                validate: ['required', 'string', 'max:50'],
-                name: 'first_name',
-            )
-            ->text(
-                label: 'What is your last name?',
-                required: true,
-                validate: ['required', 'string', 'max:50'],
-                name: 'last_name',
-            )
-            ->text(
-                label: 'What is your email?',
-                required: true,
-                validate: ['required', 'string', 'email', 'max:255', 'unique:users,email'],
-                name: 'email',
-            )
-            ->password(
-                label: 'What is your password?',
-                required: true,
-                validate: ['password' => Password::default()],
-                hint: 'Minimum 8 characters.',
-                name: 'password',
-            )
-            ->submit();
+        $isInteractive = stream_isatty(STDIN);
 
-        $data['email_verified_at'] = now();
+        if ($isInteractive) {
+            $data = form()
+                ->text(
+                    label: 'What is your first name?',
+                    required: true,
+                    validate: ['required', 'string', 'max:50'],
+                    name: 'first_name',
+                )
+                ->text(
+                    label: 'What is your last name?',
+                    required: true,
+                    validate: ['required', 'string', 'max:50'],
+                    name: 'last_name',
+                )
+                ->text(
+                    label: 'What is your email?',
+                    required: true,
+                    validate: ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+                    name: 'email',
+                )
+                ->password(
+                    label: 'What is your password?',
+                    required: true,
+                    validate: ['password' => Password::default()],
+                    hint: 'Minimum 8 characters.',
+                    name: 'password',
+                )
+                ->submit();
+        } else {
+            $data = [
+                'first_name' => $this->option('first_name') ?? throw new InvalidArgumentException('First name is required.'),
+                'last_name' => $this->option('last_name') ?? throw new InvalidArgumentException('Last name is required.'),
+                'email' => $this->option('email') ?? throw new InvalidArgumentException('Email is required.'),
+                'password' => $this->option('password') ?? throw new InvalidArgumentException('Password is required.'),
+            ];
+        }
 
-        $user = $this->createUserAction->execute($data);
+        $user = $this->createUserAction->execute([
+            ...$data,
+            'email_verified_at' => now(),
+        ]);
 
         $this->info(sprintf('User "%s" created successfully.', $user->email));
     }
